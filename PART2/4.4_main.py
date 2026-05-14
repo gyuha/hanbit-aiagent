37f6a1c2fe8d5fa2b8779b77bcb71ed772882ad5
gyuha <nicegyuha@gmail.com>
2026-05-15 00:03:42 +0900

    Refactor code structure for improved readability and maintainability

--- PART2/4.4_main.py
+++ PART2/4.4_main.py
@@ -9,7 +9,7 @@ messages = [
         "system",
         "당신은 사용자가 한 말을 영어로 번역하는 유능한 번역가입니다.",
     ),
-    ("human", "안녕하세요."),
+    ("human", "안녕하세요. 오늘의 날씨는 어떻습니까? 기분은 어때요?"),
 ]
 ai_msg = llm.invoke(messages)
 print(ai_msg)
